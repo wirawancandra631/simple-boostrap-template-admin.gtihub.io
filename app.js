74bb32b6bb1cc414ea3faa442d24cd8904329497
wirawancandra <wirawancandra631@gmail.com>
2024-10-04 22:49:54 +0700

    update-style

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
-const sidebar =document.querySelector(".sidebar");
-const btnOpenSidebar=document.querySelector("#btn-open-sidebar")
-const btnCloseSidebar=document.querySelector("#btn-close-sidebar")
+(function(){
+    const sidebar = document.querySelector(".sidebar");
+const btnOpenSidebar = document.querySelector("#btn-open-sidebar")
+const btnCloseSidebar = document.querySelector("#btn-close-sidebar")
+const mainDashboard = document.querySelector(".main-dashboard");
+let statusSidebar="show";
+
+btnOpenSidebar.addEventListener("click", function () {
+    if (window.innerWidth > 768) {
+        if (statusSidebar == "show") {
+            sidebar.style.transform = "translateX(-100%)";
+            mainDashboard.style.width = "100%";
+            mainDashboard.style.marginLeft = "0"
+            statusSidebar="hide"
 
 
-btnOpenSidebar.addEventListener("click",function(){
-    sidebar.style.display="block"
-})
-btnCloseSidebar.addEventListener("click",function(){
-    if(window.innerWidth <= 768){
-        sidebar.style.display="none";
+        }
+        else if (statusSidebar == "hide") {
+            if (window.innerWidth > 768 && window.innerWidth <= 992) {
+                mainDashboard.style.width = "70%";
+                mainDashboard.style.marginLeft = "30%"
+            }
+            else {
+                mainDashboard.style.width = "80%";
+                mainDashboard.style.marginLeft = "20%"
+            }
+            sidebar.style.transform = "translateX(0)";
+            statusSidebar="show";
+        }
+    }
+    else {
+            sidebar.style.transform="translateX(0)";
 
     }
+
+})
+btnCloseSidebar.addEventListener("click", function () {
+    if (window.innerWidth <= 768) {
+        sidebar.style.transform = "translateX(-100%)"
+
+    }
+
 })
 
+
+})()
